49b7ce97e82a9677efb6b8ea7f29a8d0d5dc4dd6
Alessandro Suha <plarpoon@plarp.net>
2026-03-26 14:47:58 +0100

    chore: restructure flake.nix

--- flake.nix
+++ flake.nix
@@ -2,34 +2,42 @@
   description = "Multi-platform Nix configuration";
 
   inputs = {
+    # NixOS
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
-
     home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    # Darwin
+    nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
     home-manager-darwin.url = "github:nix-community/home-manager/release-25.11";
     home-manager-darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
-
     nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
-
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
   };
 
   outputs =
     {
       nixpkgs,
-      nix-darwin,
       home-manager,
       home-manager-darwin,
+      nix-darwin,
       nix-homebrew,
       ...
     }:
     let
-      lib = import ./systems/lib.nix { inherit nixpkgs nix-darwin home-manager home-manager-darwin nix-homebrew; };
+      lib = import ./systems/lib.nix {
+        inherit
+          nixpkgs
+          nix-darwin
+          home-manager
+          home-manager-darwin
+          nix-homebrew
+          ;
+      };
     in
     {
+      # NixOS hosts
       nixosConfigurations = {
         daisy = lib.mkNixOS {
           hostname = "daisy";
@@ -38,6 +46,7 @@
         };
       };
 
+      # Darwin hosts
       darwinConfigurations = {
         bunnybook_air = lib.mkDarwin {
           hostname = "bunnybook_air";
